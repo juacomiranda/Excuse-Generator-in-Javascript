53b0007b1e977ddf50c5bfd1390dcb024d585afa
juacomiranda <jmiranda@live.cl>
2023-05-03 13:10:37 +0000

    terminado con node 16.13.0

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   document.querySelector("#excuse").innerHTML =
     who[whoIndex] +
     " " +
@@ -14,15 +14,20 @@ window.onload = function() {
     obj[objIndex] +
     " " +
     when[whenIndex];
-  //write your code here
 };
 
-  let who = ['Mi Perro','Mi Gata','El Vecino','Mi suegra'];
-  let action = ['botó','perdió','quebró','mojó'];
-  let what = ['el auto', 'los lentes', 'la cama'];
-  let when = ['anoche','antes de ayer','en el almuerzo','en la tarde','a la hora del té'];
-  
-  let whoIndex = Math.floor(Math.random() * who.length);
-  let whatIndex = Math.floor(Math.random() * what.length);
-  let objIndex = Math.floor(Math.random() * obj.length);
-  let whenIndex = Math.floor(Math.random() * when.length);
+let who = ["Mi Perro", "Mi Gata", "El Vecino", "Mi suegra"];
+let what = ["botó", "perdió", "quebró", "mojó"];
+let obj = ["el auto", "los lentes", "la cama"];
+let when = [
+  "anoche",
+  "antes de ayer",
+  "en el almuerzo",
+  "en la tarde",
+  "a la hora del té"
+];
+
+let whoIndex = Math.floor(Math.random() * who.length);
+let whatIndex = Math.floor(Math.random() * what.length);
+let objIndex = Math.floor(Math.random() * obj.length);
+let whenIndex = Math.floor(Math.random() * when.length);
